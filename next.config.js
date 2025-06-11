98197d1b36a33dab88e6b291c62a71d4670000dc
tony <tony@ibesttools.com>
2025-06-11 23:11:30 +0800

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,21 +2,25 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["upcdn.io", "replicate.delivery", "lh3.googleusercontent.com"],
+    domains: ['upcdn.io', 'replicate.delivery', 'lh3.googleusercontent.com'],
     unoptimized: true,
   },
   async redirects() {
     return [
       {
-        source: "/github",
-        destination: "https://github.com/Nutlope/restorePhotos",
+        source: '/github',
+        destination: 'https://github.com/laoshucheng/restorephotos',
         permanent: false,
       },
       {
-        source: "/deploy",
-        destination: "https://vercel.com/templates/next.js/ai-photo-restorer",
+        source: '/deploy',
+        destination: 'https://vercel.com/templates/next.js/ai-photo-restorer',
         permanent: false,
       },
     ];
+  },
+  webpack: (config) => {
+    config.output.chunkFilename = `[name]-[chunkhash]-${Date.now()}.js`;
+    return config;
   },
 };
