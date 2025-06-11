90d7ebfe843c5f741a6bd75101fce0658788c157
tony <tony@ibesttools.com>
2025-06-11 23:38:17 +0800

    update2

--- utils/nsfwCheck.ts
+++ utils/nsfwCheck.ts
@@ -13,13 +13,16 @@ class NSFWPredictor {
     console.log('Loading model...');
     try {
       this.model = await nsfwjs.load(
-        'https://cdn.jsdelivr.net/gh/infinitered/nsfwjs@master/models/mobilenet_v2/'
+        '/model.json', // 相对路径
+        {
+          base: 'https://cdn.jsdelivr.net/gh/infinitered/nsfwjs@master/models/mobilenet_v2/',
+          cache: false, // 禁用缓存
+        }
       );
     } catch (error) {
       console.error('Model loading failed:', error);
     }
   }
-
   predict(element: HTMLImageElement, guesses: number) {
     if (!this.model) {
       throw new Error('Some error occured, please try again later!');
